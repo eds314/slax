94f5d84806096bbee42034a1c16261a0e6c9cd49
eds314 <56859998+eds314@users.noreply.github.com>
2024-10-08 15:42:03 -0500

    users can delete messages

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,6 +1,7 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
+  alias Slax.Accounts.User
   alias Slax.Chat
   alias Slax.Chat.{Message, Room}
 
@@ -88,7 +89,7 @@ defmodule SlaxWeb.ChatRoomLive do
           </ul>
         </div>
         <div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
-          <.message :for= {{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} timezone={@timezone}/>
+          <.message :for= {{dom_id, message} <- @streams.messages} current_user={@current_user} dom_id={dom_id} message={message} timezone={@timezone}/>
         </div>
       </div>
       <div class="h-12 bg-white px-4 pb-4">
@@ -117,6 +118,7 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
+  attr :current_user, User, required: true
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
   attr :timezone, :string, required: true
@@ -124,6 +126,15 @@ defmodule SlaxWeb.ChatRoomLive do
   defp message(assigns) do
     ~H"""
     <div id={@dom_id} class="relative flex px-4 py-3">
+    <button
+      :if={@current_user.id == @message.user_id}
+      class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer"
+      data-confirm="Are you sure you want to delete your previous message?"
+      phx-click="delete-message"
+      phx-value-id={@message.id}
+      >
+        <.icon name="hero-trash" class="h-4 w-4" />
+        </button>
       <div class="h-10 w-10 rounded flex-shrink-0 bg-slate-300"></div>
       <div class="ml-2">
         <div class="-mt-1">
@@ -204,6 +215,12 @@ defmodule SlaxWeb.ChatRoomLive do
 
   defp assign_message_form(socket, changeset) do
     assign(socket, :new_message_form, to_form(changeset))
+  end
+
+  def handle_event("delete-message", %{"id" => id}, socket) do
+    {:ok, message} = Chat.delete_message_by_id(id, socket.assigns.current_user)
+
+    {:noreply, stream_delete(socket, :messages, message)}
   end
 
   def handle_event("submit-message", %{"message" => message_params}, socket) do
