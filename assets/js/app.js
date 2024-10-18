b3e0e2608b3e873c733acb438d2ee5030a5b8b3e
eds314 <56859998+eds314@users.noreply.github.com>
2024-10-17 20:38:08 -0500

    UI improvements with hooks + Enter to send message

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,9 +21,17 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import RoomMessages from "./hooks/RoomMessages"
+import ChatMessageTextArea from "./hooks/ChatMessageTextArea"
+
+const hooks = {
+  ChatMessageTextArea,
+  RoomMessages,
+}
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
+  hooks,
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken, 
   timezone: Intl.DateTimeFormat().resolvedOptions().timeZone}
