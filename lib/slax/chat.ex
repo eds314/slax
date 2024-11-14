8775d44ab2f4cd6f3a8c97e7ed253e56cd8c116b
eds314 <56859998+eds314@users.noreply.github.com>
2024-11-13 20:22:32 -0600

    toggle room membership on index page

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -102,5 +102,17 @@ defmodule Slax.Chat do
     )
   end
 
+  def toggle_room_membership(room, user) do
+    case Repo.get_by(RoomMembership, room_id: room.id, user_id: user.id) do
+      %RoomMembership{} = membership ->
+        Repo.delete(membership)
+        {room, false}
+
+      nil ->
+        join_room!(room, user)
+        {room, true}
+    end
+  end
+
   defp topic(room_id), do: "chat_room:#{room_id}"
 end
