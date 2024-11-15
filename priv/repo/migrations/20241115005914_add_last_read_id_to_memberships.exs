508c9da9b49cdd3dce7b55e5ff3c0a2844ca3ec3
eds314 <56859998+eds314@users.noreply.github.com>
2024-11-14 20:41:26 -0600

    add unread messages divider

---
+++ priv/repo/migrations/20241115005914_add_last_read_id_to_memberships.exs
@@ -0,0 +1,10 @@
+defmodule Slax.Repo.Migrations.AddLastReadIdToMemberships do
+  use Ecto.Migration
+
+  def change do
+
+    alter table(:room_memberships) do
+      add :last_read_id, :integer
+    end
+  end
+end
